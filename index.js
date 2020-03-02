1a21448cf400c61d16ea7b2f68dd0a023899e1aa
Yufei liu <liuyufei2018@gmail.com>
2020-03-02 06:43:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     
  if (katzDeli.length > 0);
 katzDeli.push(newCustomer);
- return ('Welcome, ' + newCustomer + '. You are number ' + katzDeliLine.length + ' in line.')
+ return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
    }
  
  function nowServing (katzDeliLine) {
