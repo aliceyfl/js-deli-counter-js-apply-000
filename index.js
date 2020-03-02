f0ba0e58c048211f0f87dc70c6a5476232ceaa71
Yufei liu <liuyufei2018@gmail.com>
2020-03-02 06:43:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,3 @@ katzDeli.push(newCustomer);
  return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
    }
  
- function nowServing (katzDeliLine) {
-   if(katzDeliLine.length === 0) 
-   return 'There is nobody waiting to be served!'; 
-   }
- var customer = katzDeliLine.shift(); 
- return "Currently serving " + customer + "."; 
